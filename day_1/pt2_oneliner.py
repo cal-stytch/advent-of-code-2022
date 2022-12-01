874a4285103292d4c1920e452bcf3f30d753831e
Cal Rueb <cal@stytch.com>
2022-12-01 13:58:34 -0800

    Day 1 better code golf

--- day_1/pt2_oneliner.py
+++ day_1/pt2_oneliner.py
@@ -1,2 +1,7 @@
-# Did this after the fact just to remind myself some python tricks for later days
-print(sum(sorted(map(lambda i: sum(i), map(lambda s: map(lambda n: int(n), s.split()), open('input.txt').read().split('\n\n'))))[-3:]))
+# Did this code golf after the fact just to remind myself some python tricks for later days
+
+# v1
+# print(sum(sorted(map(lambda i: sum(i), map(lambda s: map(lambda n: int(n), s.split()), open('i').read().split('\n\n'))))[-3:]))
+
+# v2    
+print(sum(sorted([sum([int(s) for s in t.split()]) for t in open('i').read().split('\n\n')])[-3:]))
