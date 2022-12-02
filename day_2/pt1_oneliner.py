2e8db6ae858f35498fde59976ee354548453aa5c
Cal Rueb <cal@stytch.com>
2022-12-02 14:31:55 -0800

    more messing around

--- day_2/pt1_oneliner.py
+++ day_2/pt1_oneliner.py
@@ -1,2 +1,7 @@
 # Magic math
 print(sum([(((((3+(r[1]-r[0]))%3)+1)%3)*3+r[1])for r in[[z[0]-64,z[1]-87]for z in[[(ord(y))for y in x.split()]for x in open('i').read().split('\n')]]]))
+
+
+# More messing around
+# print(sum([(((((3+(r[1]-r[0]))%3)+1)%3)*3+r[1])for r in[[ord(z[0])-64,ord(z[1])-87] for z in[x.split()for x in open('i').read().split('\n')]]]))
+# (x:=open('i').read().split(),print(sum([(((((3+(r[1]-r[0]))%3)+1)%3)*3+r[1]) for r in [[ord(z[0])-64,ord(z[1])-87] for z in zip(x[0::2],x[1::2])]])))
