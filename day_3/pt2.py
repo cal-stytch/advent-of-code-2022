fdc5f6d482a91f4b7457df6414d85d0f745cf7b8
Cal Rueb <cal@stytch.com>
2022-12-03 17:04:11 -0800

    Some tweaks

--- day_3/pt2.py
+++ day_3/pt2.py
@@ -2,7 +2,7 @@ file_name = 'i.txt'
 # file_name = 'test.txt'
 
 raw_input = open(file_name).read()
-raw_sacks = raw_input.split()
+raw_sacks = list(map(set, raw_input.split()))
 
 def score_char(c):
     if ord(c) >= ord('A') and ord(c) <= ord('Z'):
@@ -12,6 +12,7 @@ def score_char(c):
 # Split sacks into groups of three and find overlap via sets
 groups = []
 group = []
+
 for sack in raw_sacks:
     if (len(group) == 3):
         overlap = group[0].intersection(group[1].intersection(group[2]))
@@ -24,3 +25,8 @@ groups.append(overlap)
     
 total = sum([score_char(list(g)[0]) for g in groups])
 print(total)
+
+# Later updates for learning (able to fully replace for-loop)
+f = [raw_sacks[i].intersection(raw_sacks[i+1]).intersection(raw_sacks[i+2]) for i in range(0, len(raw_sacks), 3)]
+total_new = sum([score_char(list(g)[0]) for g in f]) 
+print(total_new)
