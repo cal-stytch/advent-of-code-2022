28d3a7ed01046e3e816556de72a4891b13bd8a70
Cal Rueb <cal@stytch.com>
2022-12-05 11:58:41 -0800

    Day 5 part 2

---
+++ day_5/pt2.py
@@ -0,0 +1,46 @@
+def parse_crate_input(s):
+    # Returns Array where index maps to row in input and stores array of crates
+    s2 = s.split('\n')
+    s3 = []
+    for t in s2:
+        s3.append([t[x+1] for x in range(0, len(t)-2, 4)])
+    # Drop the row number data
+    s3.pop()
+    s3.reverse()
+
+    crates = [ [] for _ in range(len(s3)+1)]
+    
+    for row_index, row in enumerate(s3):
+        for item_index, item in enumerate(row):
+            if (item != ' '):
+                crates[item_index].append(item)
+
+    return crates
+
+def parse_move_input(s):
+    lines = s.split('\n')
+    return [[int(x.split()[1]), int(x.split()[3]), int(x.split()[5])] for x in lines]
+
+
+def perform_move(crates, move):
+    # Return resulting move array
+    stack = crates[move[1]-1][ move[0]* -1: ]
+    crates[move[1]-1] = crates[move[1]-1][ :move[0]* -1 ]
+    crates[move[2]-1] = crates[move[2]-1] + stack
+
+    return crates 
+
+def print_cargo(cargo):
+    for r in cargo:
+       print(r) 
+    print('---------')
+
+contents = open('input.txt').read()
+contents = contents.split('\n\n')
+crates = parse_crate_input(contents[0])
+moves = parse_move_input(contents[1])
+
+for move in moves:
+    crates = perform_move(crates, move)
+
+print(''.join([a[-1] for a in crates]))
